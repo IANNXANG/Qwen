791689d64ba13b679323707ed63b2bbd4bdbdb6d
IANNXANG <yx17180324@gmail.com>
2024-10-18 23:46:22 +0800

    .TO

--- Math-s.py
+++ Math-s.py
@@ -5,6 +5,7 @@ import torch
 good_token = '+'
 bad_token = '-'
 step_tag = 'ки'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model_path = "/pubshare/LLM/math-shepherd-mistral-7b-prm"
 
@@ -19,6 +20,7 @@ print(step_tag_id)
 print("---------------------------------------------")
 
 model = AutoModelForCausalLM.from_pretrained(model_path).eval()
+model.to(device)
 
 question = """Janet\u2019s ducks lay 16 eggs per day. She eats three for breakfast every morning and bakes muffins for her friends every day with four. She sells the remainder at the farmers' market daily for $2 per fresh duck egg. How much in dollars does she make every day at the farmers' market?"""
 output1 = """Step 1: Janet's ducks lay 16 eggs per day. ки\nStep 2: She eats three for breakfast every morning, so she has 16 - 3 = 13 eggs left. ки\nStep 3: She bakes muffins for her friends every day with four eggs, so she has 13 - 4 = 9 eggs left. ки\nStep 4: She sells the remainder at the farmers' market daily for $2 per fresh duck egg, so she makes 9 * $2 = $18 every day at the farmers' market. The answer is: 18 ки"""  # 18 is right
@@ -41,16 +43,14 @@ for i in range(1, n+1):
     value = eval(output_name)
     oplist.append(value)
 
-
-
 for output in oplist:
     input_for_prm = f"{question} {output}"
-    input_id = torch.tensor([tokenizer.encode(input_for_prm)])
+    input_id = torch.tensor([tokenizer.encode(input_for_prm)]).to(device)
 
     with torch.no_grad():
-        logits = model(input_id).logits[:, :, candidate_tokens]
-        scores = logits.softmax(dim=-1)[:, :, 0]
-        step_scores = scores[input_id == step_tag_id]
+        logits = model(input_id).logits[:, :, candidate_tokens].to(device)
+        scores = logits.softmax(dim=-1)[:, :, 0].to(device)
+        step_scores = scores[input_id == step_tag_id].to(device)
         print(step_scores)
 
 # tensor([0.9955, 0.9958, 0.9983, 0.9957])
