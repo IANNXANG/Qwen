868a5f39820a524d06d94abfc34c28503863b1ae
IANNXANG <yx17180324@gmail.com>
2024-10-22 16:22:29 +0800

    分词

--- PRM_ms.py
+++ PRM_ms.py
@@ -38,7 +38,7 @@ The answer is: 17 ки"""  # 17 is wrong
 output3 = """Step 1: Janet's ducks lay 12 eggs per day. ки\nStep 2: She eats three for breakfast every morning, so she has 16 - 3 = 13 eggs left. ки\nStep 3: She bakes muffins for her friends every day with four eggs, so she has 13 - 4 = 9 eggs left. ки\nStep 4: She sells the remainder at the farmers' market daily for $2 per fresh duck egg, so she makes 9 * $2 = $17 every day at the farmers' market. The answer is: 17 ки"""
 output4 = """Step 1: Janet's ducks lay 16 eggs per day. ки\nStep 2: She bakes muffins for her friends every day with four eggs, so she has 13 - 4 = 9 eggs left. ки\nStep 3: She sells the remainder at the farmers' market daily for $2 per fresh duck egg, so she makes 9 * $2 = $17 every day at the farmers' market. The answer is: 17 ки"""
 oplist = []
-n = 4
+n = 2
 # tensor([0.9955, 0.9958, 0.9983, 0.9957])
 # tensor([0.9955, 0.9958, 0.9983, 0.0240])  把最后一个step改错
 # tensor([0.3700, 0.6413, 0.8014, 0.0163])  把第一个改错
