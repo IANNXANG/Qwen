d3a762adeed56ec3c612183eca67900648c74781
IANNXANG <yx17180324@gmail.com>
2024-10-22 16:21:34 +0800

    分词

--- PRM_ms.py
+++ PRM_ms.py
@@ -22,17 +22,27 @@ print("---------------------------------------------")
 model = AutoModelForCausalLM.from_pretrained(model_path).eval()
 model.to(device)
 
-question = """Janet\u2019s ducks lay 16 eggs per day. She eats three for breakfast every morning and bakes muffins for her friends every day with four. She sells the remainder at the farmers' market daily for $2 per fresh duck egg. How much in dollars does she make every day at the farmers' market?"""
-output1 = """Step 1: Janet's ducks lay 16 eggs per day. ки\nStep 2: She eats three for breakfast every morning, so she has 16 - 3 = 13 eggs left. ки\nStep 3: She bakes muffins for her friends every day with four eggs, so she has 13 - 4 = 9 eggs left. ки\nStep 4: She sells the remainder at the farmers' market daily for $2 per fresh duck egg, so she makes 9 * $2 = $18 every day at the farmers' market. The answer is: 18 ки"""  # 18 is right
-output2 = """Step 1: Janet's ducks lay 16 eggs per day. ки\nStep 2: She eats three for breakfast every morning, so she has 16 - 3 = 13 eggs left. ки\nStep 3: She bakes muffins for her friends every day with four eggs, so she has 13 - 4 = 9 eggs left. ки\nStep 4: She sells the remainder at the farmers' market daily for $2 per fresh duck egg, so she makes 9 * $2 = $17 every day at the farmers' market. The answer is: 17 ки"""  # 17 is wrong
+question = """
+Janet\u2019s ducks lay 16 eggs per day. She eats three for breakfast every morning and bakes muffins for her friends every day with four. She sells the remainder at the farmers' market daily for $2 per fresh duck egg. How much in dollars does she make every day at the farmers' market?"""
+output1 = """Step 1: Janet's ducks lay 16 eggs per day. ки\n
+Step 2: She eats three for breakfast every morning, so she has 16 - 3 = 13 eggs left. ки\n
+Step 3: She bakes muffins for her friends every day with four eggs, so she has 13 - 4 = 9 eggs left. ки\n
+Step 4: She sells the remainder at the farmers' market daily for $2 per fresh duck egg, so she makes 9 * $2 = $18 every day at the farmers' market. 
+The answer is: 18 ки"""  # 18 is right
+output2 = """
+Step 1: Janet's ducks lay 16 eggs per day. ки\n
+Step 2: She eats three for breakfast every morning, so she has 16 - 3 = 13 eggs left. ки\n
+Step 3: She bakes muffins for her friends every day with four eggs, so she has 13 - 4 = 9 eggs left. ки\n
+Step 4: She sells the remainder at the farmers' market daily for $2 per fresh duck egg, so she makes 9 * $2 = $17 every day at the farmers' market. 
+The answer is: 17 ки"""  # 17 is wrong
 output3 = """Step 1: Janet's ducks lay 12 eggs per day. ки\nStep 2: She eats three for breakfast every morning, so she has 16 - 3 = 13 eggs left. ки\nStep 3: She bakes muffins for her friends every day with four eggs, so she has 13 - 4 = 9 eggs left. ки\nStep 4: She sells the remainder at the farmers' market daily for $2 per fresh duck egg, so she makes 9 * $2 = $17 every day at the farmers' market. The answer is: 17 ки"""
 output4 = """Step 1: Janet's ducks lay 16 eggs per day. ки\nStep 2: She bakes muffins for her friends every day with four eggs, so she has 13 - 4 = 9 eggs left. ки\nStep 3: She sells the remainder at the farmers' market daily for $2 per fresh duck egg, so she makes 9 * $2 = $17 every day at the farmers' market. The answer is: 17 ки"""
 oplist = []
 n = 4
 # tensor([0.9955, 0.9958, 0.9983, 0.9957])
-# tensor([0.9955, 0.9958, 0.9983, 0.0240])  最后一个错了
+# tensor([0.9955, 0.9958, 0.9983, 0.0240])  把最后一个step改错
 # tensor([0.3700, 0.6413, 0.8014, 0.0163])  把第一个改错
-# tensor([0.9955, 0.2233, 0.0090])   删除第二个step
+# tensor([0.9955, 0.2233, 0.0090])          删除第二个step
 
 
 #在q和a之间添加\n
@@ -52,6 +62,12 @@ for output in oplist:
     print("---------------------------------------------")
     print("input_for_prm:")
     print(input_for_prm)
+    # 获取分词结果但不进行编码
+    tokenized_result = tokenizer.tokenize(input_for_prm).to(device)
+    print("---------------------------------------------")
+    print("Tokenized result:")
+    print(tokenized_result)
+
     input_id = torch.tensor([tokenizer.encode(input_for_prm)]).to(device)
     print("---------------------------------------------")
     print("input_id:")
@@ -60,8 +76,8 @@ for output in oplist:
     with torch.no_grad():
         logits = model(input_id).logits[:, :, candidate_tokens].to(device)
         print("---------------------------------------------")
-        print("logits:")
-        print(logits)   #输出为+或者-的logits
+        # print("logits:")
+        # print(logits)   #输出为+或者-的logits
         scores = logits.softmax(dim=-1)[:, :, 0].to(device)
         print("---------------------------------------------")
         print("scores:")
