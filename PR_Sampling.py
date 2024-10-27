9638858214aaa3e760256e526208b9bb7295694b
IANNXANG <yx17180324@gmail.com>
2024-10-27 14:41:33 +0800

    step_scores.tolist()

--- PR_Sampling.py
+++ PR_Sampling.py
@@ -77,23 +77,3 @@ for index, data in enumerate(data_list):
 
 
 
-
-
-
-
-
-    #     if index > 0:
-    #         Input4PRM = Input4PRM + part + "ки"
-    #     else:
-    #         Input4PRM = Input4PRM + part
-    # input_for_prm = Input4PRM
-    # print("input_for_prm:",input_for_prm)
-    # input_id = torch.tensor([tokenizer.encode(input_for_prm)]).to(device)
-    # with torch.no_grad():
-    #     logits = prm_model(input_id).logits[:, :, candidate_tokens].to(device)
-    #     scores = logits.softmax(dim=-1)[:, :, 0].to(device)
-    #     step_scores = scores[input_id == step_tag_id].to(device)
-    #     print("---------------------------------------------")
-    #     print("step_scores:")
-    #     print(step_scores)
-
