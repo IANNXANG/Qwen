f3e6ab0231aa86bf51a0cc6477cb56458a01d4a3
IANNXANG <yx17180324@gmail.com>
2024-10-27 11:36:20 +0800

    prm_model

--- Step_Sampling.py
+++ Step_Sampling.py
@@ -14,7 +14,7 @@ with open('train.jsonl', 'r') as file:
 question_count = len(data)
 print(f"JSON 中问题的条数为：{question_count}")
 
-model_path = "/pubshare/LLM/Qwen2.5-Math-7B-Instruct"
+model_path = "/pubshare/LLM/Qwen2.5-Math-1.5B-Instruct"
 #cache_dir = "/pubshare/LLM"
 # cache_dir = "/home/jovyan/.cache/huggingface/hub"
 # 加载模型和分词器
@@ -26,6 +26,26 @@ model = AutoModelForCausalLM.from_pretrained(model_path).eval()
 # 设置模型运行环境
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
+
+prm_model_path = "/pubshare/LLM/math-shepherd-mistral-7b-prm"
+good_token = '+'
+bad_token = '-'
+step_tag = 'ки'
+prm_tokenizer = AutoTokenizer.from_pretrained(prm_model_path)
+candidate_tokens = prm_tokenizer.encode(f"{good_token} {bad_token}")[1:]  # [648, 387]
+step_tag_id = prm_tokenizer.encode(f"{step_tag}")[-1]  # 12902
+print("---------------------------------------------")
+print("判断符:",candidate_tokens)
+print("分隔符:",step_tag_id)
+# [648, 387]
+# 12902
+print("---------------------------------------------")
+prm_model = AutoModelForCausalLM.from_pretrained(model_path).eval()
+prm_model.to(device)
+
+
+
+
 
 # 打印读取到的 JSON 数据
 for item in data:
@@ -46,5 +66,4 @@ for item in data:
         key = f"step{index}" if index > 0 else "question"
         result_dict[key] = part
     print("------------------------------------------------------------------------------------")
-    input("Press Enter to continue:")
     pprint.pprint(result_dict)
