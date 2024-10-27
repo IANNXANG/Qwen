496e11533eb7741f4fe0e95cb1a0c27d6f774c11
IANNXANG <yx17180324@gmail.com>
2024-10-27 17:22:02 +0800

    4eval

--- PR_Sampling4eval.py
+++ PR_Sampling4eval.py
@@ -42,6 +42,7 @@ model.to(device)
 for index, data in enumerate(data_list):
     print(f"Data {index + 1}:")
     input_for_prm = data["question"]+"\n"+data["solution"]
+    input_for_prm = input_for_prm.replace('\n\n', 'ки')
     print(input_for_prm)
 
     input_id = torch.tensor([tokenizer.encode(input_for_prm)]).to(device)
