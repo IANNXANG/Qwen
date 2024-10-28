d3e5a92146b365f8a92480fdf986bf1e3ae69c5b
IANNXANG <yx17180324@gmail.com>
2024-10-28 22:05:25 +0800

    PRS

--- PR_Sampling.py
+++ PR_Sampling.py
@@ -14,7 +14,7 @@ work = args.work
 # 初始化一个空列表来存储数据
 data_list = []
 
-file_name = f'collect/{work}.jsonl'
+file_name = f'collect/result_{work}_full.jsonl'
 # 读取jsonl文件
 with open(file_name, 'r', encoding='utf-8') as file:
     for line in file:
