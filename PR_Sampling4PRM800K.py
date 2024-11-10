5288e8a710030f80a21e87b6b62564db0901184e
IANNXANG <yx17180324@gmail.com>
2024-11-10 18:44:38 +0800

    PRM800K

---
+++ PR_Sampling4PRM800K.py
@@ -0,0 +1,81 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import json
+import argparse
+
+# 添加命令行参数解析
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--work', type=str, default="phase2_test", help='The work directory')
+
+args = parser.parse_args()
+work = args.work
+
+
+# 初始化一个空列表来存储数据
+data_list = []
+file_name = f"../PRM800k/{work}.jsonl"
+# 读取jsonl文件
+with open(file_name, 'r', encoding='utf-8') as file:
+    for line in file:
+        # 解析每一行的JSON数据
+        data = json.loads(line)
+        data_list.append(data)
+
+question_count = len(data_list)
+print(f"JSON 中问题的条数为：{question_count}")
+
+good_token = '+'
+bad_token = '-'
+step_tag = 'ки'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_path = "/pubshare/LLM/math-shepherd-mistral-7b-prm"
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+candidate_tokens = tokenizer.encode(f"{good_token} {bad_token}")[1:]  # [648, 387]
+step_tag_id = tokenizer.encode(f"{step_tag}")[-1]  # 12902
+step_tag_id = 1107
+print("---------------------------------------------")
+print("判断符:",candidate_tokens)
+print("分隔符:",step_tag_id)
+# [648, 387]
+# 12902
+print("---------------------------------------------")
+model = AutoModelForCausalLM.from_pretrained(model_path).eval()
+model.to(device)
+
+
+for index, data in enumerate(data_list):
+    print(f"Data {index + 1}:")
+    problem = data['question']['problem']
+    input_for_prm = ""
+    for step in data['label']['steps']:
+        for completion in step['completions']:
+            input_for_prm += completion['text']
+        input_for_prm += "ки"
+    input_for_prm = problem + "\n" + input_for_prm
+    print(input_for_prm)
+
+    input_id = torch.tensor([tokenizer.encode(input_for_prm)]).to(device)
+    print("---------------------------------------------")
+
+
+    with torch.no_grad():
+        logits = model(input_id).logits[:, :, candidate_tokens].to(device)
+        scores = logits.softmax(dim=-1)[:, :, 0].to(device)
+        step_scores = scores[input_id == step_tag_id].to(device)
+        print("---------------------------------------------")
+        print("step_scores:")
+        print(step_scores)
+        result_dict = {}
+        step_scores = step_scores.tolist()
+        for index_step, score in enumerate(step_scores):
+            key = f"step{index_step+1}_score"
+            result_dict[key] = score
+            print(score)
+        with open(f'/home/jovyan/notebook/zhouyang/{work}_score.jsonl', 'a') as file:
+            json.dump(result_dict, file)
+            file.write('\n')
+
+
+
