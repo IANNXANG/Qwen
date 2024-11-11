548cc3ef29daca2f0dfdaabccacaa19ce3ee0598
IANNXANG <yx17180324@gmail.com>
2024-11-11 16:23:57 +0800

    prompt

--- peiyi9979mistral_7b_sft.py
+++ peiyi9979mistral_7b_sft.py
@@ -20,13 +20,12 @@ model = AutoModelForCausalLM.from_pretrained("peiyi9979/mistral-7b-sft").eval()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-# 打印读取到的 JSON 数据
-for item in data:
-    print("------------------------------------------------------------------------------------")
-    print(f"问题：{item['question']}\n答案：{item['answer']}")
+prompt = ("Chandra has four bowls.  Each one is a different color (red, blue, yellow, green).  She also has exactly"
+          " one glass the same color as each bowl.  If she chooses a bowl and a glass from the cupboard, how many pa"
+          "irings are possible?  One such pairing is a blue bowl and a yellow glass.")
 
-    # 调用模型回答问题
-    inputs = tokenizer.encode(item['question'], return_tensors='pt').to(device)
-    outputs = model.generate(inputs, max_length=1024, num_return_sequences=1)
-    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    print(f"模型生成的答案：{generated_text}")
+# 调用模型回答问题
+inputs = tokenizer.encode(prompt, return_tensors='pt').to(device)
+outputs = model.generate(inputs, max_length=1024, num_return_sequences=1)
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+print(f"模型生成的答案：{generated_text}")
